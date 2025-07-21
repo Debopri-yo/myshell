2e504eb2f1cc65fbc71933940291b1064f3a6890
Debopri-yo <ghoshlipika53@gmail.com>
2025-07-22 03:49:23 +0530

    add the fork system call in the source code

--- src/shell.cpp
+++ src/shell.cpp
@@ -1,5 +1,8 @@
 #include<iostream>
 #include<string>
+#include<unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 using namespace std;
 int main(){
 	string input;
@@ -18,9 +21,21 @@ int main(){
 		cout<<"goodbye"<<endl;
 		break;
 	}
-	cout<<"you typed"<<input<<endl;
-
+	pid_t pid=fork();
+	if (pid<0){
+		cerr<<"fork failed"<<endl;
 	}
+	else if (pid==0){
+		cout<<"[Child] PID: "<<getpid()<<"Running | "<<input<<endl;
+		_exit(0);
+	}
+	else{
+		int status;
+		wait(&status);
+		cout<<"[Parent] child finished with status"<<status<<endl;
+	}
+}
 return 0;
 }
+
 
