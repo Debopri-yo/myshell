d1f3a09d33c7915f0499d3cfbf3b68b7a5f27a41
Debopri-yo <ghoshlipika53@gmail.com>
2026-09-04 03:46:49 +0530

    fix history bug

--- src/myshell.c
+++ src/myshell.c
@@ -4,11 +4,66 @@
 #include <unistd.h>
 #include <string.h>
 #include <ctype.h>
-
+#include <libgen.h>
+#define HISTORY_FILE "history.txt"
 #define MAX_LINE 1024
 #define MAX_ARGS 64
 #define MAX_HISTORY 5
+void get_history_path(char *buffer, size_t size) {
+    char path[MAX_LINE];
+    ssize_t len = readlink("/proc/self/exe", path, sizeof(path) - 1);
+    if (len == -1) {
+        perror("readlink");
+        exit(1);
+    }
+    path[len] = '\0';
+    char *dir = dirname(path);
+    snprintf(buffer, size, "%s/%s", dir, HISTORY_FILE);
+}
+void saveHistory(char history[][MAX_LINE], int history_count, char *path){    
+    FILE *file = fopen(path, "w");
+    if (file == NULL) {
+        perror("Could not open history file");
+        return;
+    }
+    int start = (history_count > MAX_HISTORY) ? (history_count - MAX_HISTORY) : 0;
+    for (int i=start;i< history_count;i++){
+        int index = i % MAX_HISTORY;
+        fprintf(file, "%d. %s\n", i + 1, history[index]);
+    }
+    fclose(file);
+}
+void loadHistory(char history[][MAX_LINE], int *history_count, char *path) {
+    FILE *file = fopen(path, "r");
+    if (file == NULL) {
+        return;
+    }
+    char line[MAX_LINE];
+    int last_num = 0;
+    while (fgets(line, sizeof(line), file)) {
+        line[strcspn(line, "\n")] = 0;
 
+        char *dot = strchr(line, '.');
+        int num;
+        char *cmd;
+        if (dot && dot[1] == ' ') {
+            num = atoi(line);
+            cmd = dot + 2;
+        } else {
+            // malformed/legacy line, skip numbering assumption
+            num = last_num + 1;
+            cmd = line;
+        }
+
+        int index = (num - 1) % MAX_HISTORY;
+        strncpy(history[index], cmd, MAX_LINE - 1);
+        history[index][MAX_LINE - 1] = '\0';
+
+        if (num > last_num) last_num = num;
+    }
+    *history_count = last_num;
+    fclose(file);
+}
 void parse_input(char *input, char **args) {
     int i = 0;
     args[i] = strtok(input, " \t\n");
@@ -26,15 +81,20 @@ int main() {
     int status;
     int history_count = 0;
     char cwd[MAX_LINE];
-
+    char path[MAX_LINE];
+    get_history_path(path, sizeof(path));
+    loadHistory(history, &history_count, path);
     while (1) {
-        getcwd(cwd, sizeof(cwd));
+        if (!(getcwd(cwd, sizeof(cwd)))) {
+            perror("getcwd failed");
+            continue;
+        }
         printf("myshell:~%s:", cwd);
-
         if (!fgets(input, MAX_LINE, stdin)) {
+           saveHistory(history, history_count, path);
             break;
         }
-
+	//!number functionality start
         if (input[0] == '!' && input[1] != '\0') {
             int valid = 1;
             for (int i = 1; input[i] != '\n' && input[i] != '\0'; i++) {
@@ -51,7 +111,8 @@ int main() {
 
                 if (cmd_num >= min_cmd && cmd_num <= max_cmd) {
                     int index = (cmd_num - 1) % MAX_HISTORY;
-                    strcpy(input, history[index]);
+                    strncpy(input, history[index], MAX_LINE - 1);
+                    input[MAX_LINE - 1] = '\0'; // ensure null termination
                     goto process_input;
                 } else {
                     printf("No such command in history\n");
@@ -59,6 +120,7 @@ int main() {
                 }
             }
         }
+	//!number functionality end
         process_input:
         size_t len = strlen(input);
         if (len > 0 && input[len - 1] == '\n') {
@@ -76,6 +138,7 @@ int main() {
         }
 
         if (strcmp(args[0], "exit") == 0 || strcmp(args[0], "quit") == 0) {
+            saveHistory(history, history_count, path);
             break;
         }
 
